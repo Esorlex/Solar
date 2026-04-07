b70321f614406c35729e9ec2614628fc54a86023
esorlex <147352431+Esorlex@users.noreply.github.com>
2026-04-07 10:05:59 -0400

    Delete hi.bat

--- hi.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Worked!','Warning',[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Warning)"
